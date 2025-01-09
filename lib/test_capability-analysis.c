4176b2b4abef1a6e0665411f3cddee1243fcf6e9
Marco Elver <elver@google.com>
2025-01-09 16:36:12 +0100

    compiler-capability-analysis: Add test stub
    WIP
    
    Signed-off-by: Marco Elver <elver@google.com>

---
+++ lib/test_capability-analysis.c
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Compile-only tests for common patterns that should not generate false
+ * positive errors when compiled with Clang's capability analysis.
+ */
